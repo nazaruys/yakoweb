619359a6d2a5164570410150980d6ec81b60c5e1
nazaruys <nazary.box@gmail.com>
2025-07-11 09:39:34 +0200

    Test

--- app/layout.js
+++ app/layout.js
@@ -4,22 +4,20 @@ import { FontProvider } from './components/FontProvider';
 import { AnalyticsScript } from './components/AnalyticsScript';
 import { LayoutShell } from './components/LayoutShell';
 import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
 import CookieBanner from './components/CookieBanner';
+import SetHtmlLang from './components/SetHtmlLang';
 // import { metadata } from './metadata';
 
 // export { metadata };
 
-export default async function RootLayout({ children, params }) {
-  const locale = params?.locale || 'en';
-  const messages = await getMessages();
-
+export default async function RootLayout({ children }) {
   return (
-    <html lang={locale}>
+    <html>
       <body>
+        <SetHtmlLang lang={"nl"}/>
         <FontProvider>
           <AnalyticsScript />
-          <NextIntlClientProvider locale={locale} messages={messages}>
+          <NextIntlClientProvider>
             <LayoutShell>
               {children}
             </LayoutShell>
