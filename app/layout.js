8a5dd081db40c412762e905fe182a8934407187f
nazaruys <nazary.box@gmail.com>
2025-07-11 13:39:10 +0200

    Added hreflang

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,19 @@ import CookieBanner from './components/CookieBanner';
 export default async function RootLayout({ children }) {
   return (
     <html lang="nl">
+      <head>
+        <link rel="alternate" href="https://www.yakoweb.com/" hreflang="nl" />
+        <link rel="alternate" href="https://www.yakoweb.com/privacyverklaring/" hreflang="nl" />
+        <link rel="alternate" href="https://www.yakoweb.com/algemene-voorwaarden/" hreflang="nl" />
+        <link rel="alternate" href="https://www.yakoweb.com/landing-thank-you/" hreflang="nl" />
+        <link rel="alternate" href="https://www.yakoweb.com/standard-thank-you/" hreflang="nl" />
+        <link rel="alternate" href="https://www.yakoweb.com/en/" hreflang="en" />
+        <link rel="alternate" href="https://www.yakoweb.com/privacy-policy/" hreflang="en" />
+        <link rel="alternate" href="https://www.yakoweb.com/terms-and-conditions/" hreflang="en" />
+        <link rel="alternate" href="https://www.yakoweb.com/en/landing-thank-you/" hreflang="en" />
+        <link rel="alternate" href="https://www.yakoweb.com/en/standard-thank-you/" hreflang="en" />
+        <link rel="alternate" href="https://www.yakoweb.com/" hreflang="x-default" />
+      </head>
       <body>        
         <FontProvider>
           <AnalyticsScript />
